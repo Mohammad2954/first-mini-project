8e78b2f3a2fd44a609a0ef6762e9ac5ed334149e
mohammadjavad <mohamad2134javad@gmail.com>
2025-11-27 00:25:03 +0330

    the header is complete

--- script/app.js
+++ script/app.js
@@ -1,15 +1,28 @@
 const navBtn=document.querySelector(".nav__btn")
+const navMenu=document.querySelector(".nav-menu")
+let menuLink=document.querySelectorAll(".menu_link")
 flagbtn=false
 navBtn.addEventListener("click" ,function(){
     if(flagbtn){
         navBtn.classList.remove("nav__btn--open")
+        navMenu.classList.remove("nav-menu--open")
         flagbtn=false
         
     }else{
         navBtn.classList.add("nav__btn--open")
+        navMenu.classList.add("nav-menu--open")
         flagbtn=true
         
 
     }
 
+})
+menuLink.forEach(a=>{
+    a.addEventListener("click",function(){
+        menuLink.forEach(b=>{
+            b.classList.remove("menu_link--active")
+        })
+        a.classList.add("menu_link--active")
+    })
+    
 })
